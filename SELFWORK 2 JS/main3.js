73c50dc5a9530f5954e3cc1b9e1980abed1d7418
Alessio-Salerno <156314493+Alessio-Salerno@users.noreply.github.com>
2024-06-04 17:42:52 +0200

    Add files via upload

---
+++ SELFWORK 2 JS/main3.js
@@ -0,0 +1,18 @@
+// console.log('Numeri pari da 1 a 20');
+
+// for(let i = 1; i <= 20; i++ ){
+//     if(i % 2 === 0)
+//     console.log(i)
+// }
+
+
+console.log('Numeri dispari da 1 a 20');
+
+for(let i = 1; i <= 20; i++){
+    if(i % 2 != 0 )
+      console.log(i)  
+}
+
+
+// Calcolo della media dei numeri dispari
+
